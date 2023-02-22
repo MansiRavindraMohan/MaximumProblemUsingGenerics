89b7af9e218d3feef94b7081f55df4c47585a489
MansiRavindraMohan <manasimohan29@gmail.com>
2023-02-22 13:49:08 +0530

    Welcome to maximum number problem using generics

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MaximumProblemUsingGenerics
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to maximum problem using generics");
+        }
+    }
+}
