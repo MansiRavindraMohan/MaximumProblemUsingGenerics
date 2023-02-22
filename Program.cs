719b69acf3c367067dbea5484a2accd32254b0ea
MansiRavindraMohan <manasimohan29@gmail.com>
2023-02-22 14:00:45 +0530

    Find the maximum integer among 3 integers

--- Program.cs
+++ Program.cs
@@ -4,7 +4,13 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to maximum problem using generics");
+            //Console.WriteLine("Welcome to maximum problem using generics");
+
+            int num1 = 70, num2 = 120, num3 = 100;
+
+            int value = MaximumNumber.MaximumIntegerNumber(num1, num2, num3);
+
+            Console.WriteLine(value + " is greatest");
         }
     }
 }
